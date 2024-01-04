6e50878a1a9e2e5c04779952b6fbf40f257ed7c3
Your Name <you@example.com>
2024-01-04 12:37:56 +0600

    First commit

--- fontend/src/pages/AdminAccess/SingleStudent.jsx
+++ fontend/src/pages/AdminAccess/SingleStudent.jsx
@@ -2,6 +2,7 @@ import { useEffect } from "react"
 import { useDispatch, useSelector } from "react-redux"
 import { getAdminStudent } from "../../redux/actions/adminAction"
 import { useParams } from "react-router-dom"
+import img from "../../assets/images/studing.jpg"
 
 
 const SingleStudent = () => {
@@ -13,7 +14,33 @@ const SingleStudent = () => {
         dispatch(getAdminStudent(id))
     },[id])
   return (
-    <div>SingleStudent</div>
+    <div className="px-12 pt-12 flex justify-center">
+      <div className="w-4/12 bg-slate-400 p-6 rounded-xl mt-5">
+        <div className="my-2">
+          <label className="font-poppins font-bold text-sm">Name</label>
+          <p className="border-2 border-slate-300 px-5 py-2 font-poppins font-medium text-sm  rounded-xl mt-2">
+            {student && student.name  ? student.name : "No Name Found!"}
+          </p>
+        </div>
+        <div className="my-2">
+          <label className="font-poppins font-bold text-sm">Email</label>
+          <p className="border-2 border-slate-300 px-5 py-2 font-poppins font-medium text-sm  rounded-xl mt-2">
+            {student && student.email  ? student.email : "No Email Found!"}
+          </p>
+        </div>
+        <div className="my-2">
+          <label className="font-poppins font-bold text-sm">ID</label>
+          <p className="border-2 border-slate-300 px-5 py-2 font-poppins font-medium text-sm  rounded-xl mt-2">
+            {student && student.id  ? student.id : "No ID Found!"}
+          </p>
+        </div>
+        <div className="my-4">
+        <label className="font-poppins font-bold text-sm">Avatar</label>
+         
+          {student && student.avatar ? <img src={student.avatar.url} className="w-full h-46 rounded-lg"/>:<img src={img} className="w-full h-46 rounded-lg"/>}
+        </div>
+      </div>
+    </div>
   )
 }
 
